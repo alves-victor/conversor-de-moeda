30c520aeb6d5554c73bccfb057cd9c0b390406bb
Victor Alves <victorhugo_sp4@hotmail.com>
2021-09-22 19:52:04 -0300

    configuracao do componente conversor

---
+++ src/components/Conversor.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import './Conversor.css'
+
+const Conversor = (props) => {
+    const [valorA, setValorA] = useState("")
+    const [valorB, setValorB] = useState(0)
+
+    function converter(){
+        let de_para = `${props.moedaA}_${props.moedaB}`
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=a3244e1b28ec4356defb`
+        fetch(url).then(res => {
+            return res.json()
+        }).then(json => {
+            let cotacao = json[de_para]
+            let valorB = (parseFloat(valorA) * cotacao).toFixed(2)
+            setValorB(valorB) 
+        })
+    }
+
+    return(
+        <div className="conversor">
+            <h2>{props.moedaA} para {props.moedaB}</h2>
+            <input type="text"
+                onChange={(e) => {
+                    setValorA(e.target.value)
+                }}
+            />               
+            <button onClick={converter}>Converter</button>
+            <h2>valor convertido = {valorB}</h2>
+        </div>
+    )
+}
+
+export default Conversor
